ce0e1b50ce885f48fab9af3d3985d01020140f9c
kingHawkWin <kinghawkwin@icloud.com>
2020-12-13 22:59:17 +0800

    feat(ts): singleton
    ts singleton guru_example

---
+++ typescript/creational/singleton/guru_example/guru_example.js
@@ -0,0 +1,18 @@
+"use strict";
+var Singleton = /** @class */ (function () {
+    function Singleton() {
+    }
+    Singleton.getInstance = function () {
+        if (!Singleton.instance) {
+            Singleton.instance = new Singleton();
+        }
+        return Singleton.instance;
+    };
+    return Singleton;
+}());
+function client() {
+    var s1 = Singleton.getInstance();
+    var s2 = Singleton.getInstance();
+    console.log("s1 === s2, " + (s1 === s2));
+}
+client();
